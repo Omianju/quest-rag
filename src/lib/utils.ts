b866209d89b1f4217f65396207b7da599351e811
Omianju <devansh1413@gmail.com>
2024-11-04 14:53:30 +0530

    added: construct metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -21,3 +22,46 @@ export function absoluteUrl(path: string) {
   return isServer ? localUrl : `${window.location.origin}${path}`;
 }
 
+export function constructMetadata({
+  title = "QuestRag - the SaaS for readers",
+  description = "QuestRag is a open-source software to make chatting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+  description,
+  openGraph : {
+    title,
+    description,
+    images : [
+      {
+        url : image
+      }      
+    ]
+  },
+  twitter : {
+    card : "summary_large_image",
+    title,
+    description,
+    images : [image],
+    creator : "@devansh1413@gamil.com"
+  },
+  icons,
+  metadataBase : new URL("https://quest-rag.vercel.app"),
+  themeColor : "#FFF",
+  ...(noIndex && {
+    robots : {
+      follow : false,
+      index : false
+    }
+  })
+  }
+};
